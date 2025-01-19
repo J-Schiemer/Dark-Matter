305713ff54b2741640461657caf5c662305a5868
Johannes Schiemer <johannes@schiemer.dev>
2025-01-19 21:46:58 +0100

    feat: Added dark mode provider

--- web/dark-matter-web/src/App.tsx
+++ web/dark-matter-web/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { useTheme } from './providers/theme-provider'
 
 function App() {
 
+  const toggle = useTheme();
+
   return (
     <>
-      
+      <button className='button is-primary' onClick={() => {toggle.toggle();}}>aaaa</button>
     </>
   )
 }
