9ff5024849b5ba686992087d801e09c54cc11b2a
Johannes Schiemer <johannes@schiemer.dev>
2025-01-19 18:39:40 +0100

    feat: Added NSwag and Client Generation

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -1,17 +1,18 @@
+using api;
+using NSwag.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.ConfigureServices();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseOpenApi();
+    app.UseReDoc();
 }
 
 app.UseHttpsRedirection();
